b5b8acdb845e6473b8455fb8b1beff7dc4db0ab1
iromli <isman.firmansyah@gmail.com>
2023-11-23 04:21:22 +0700

    feat: remove class-based methods in favor of functions
    Release-As: 0.5.0

--- src/sprig_aes/core.py
+++ src/sprig_aes/core.py
@@ -18,50 +18,40 @@ def _fill_bytes(value: AnyStr) -> bytes:
     return (_as_bytes(value) + bytes(32))[:32]
 
 
-class SprigAES:
-    """This class encrypt/decrypt text that is compatible with Sprig AES."""
-
-    BLOCK_SIZE = 16
-
-    @classmethod
-    def encrypt(cls, text: AnyStr, key: AnyStr) -> bytes:
-        key_bytes = _fill_bytes(key)
-        text_bytes = _as_bytes(text)
-
-        # fill the block size of text_bytes
-        padding = cls.BLOCK_SIZE - len(text_bytes) % cls.BLOCK_SIZE
-        text_bytes += bytes(chr(padding) * padding, "utf-8")
-
-        iv = os.urandom(cls.BLOCK_SIZE)
-        # Sprig Go prepend the ciphertext with iv
-        ciphertext = iv + text_bytes
-        cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv))
-
-        encryptor = cipher.encryptor()
-        encrypted_text = encryptor.update(ciphertext) + encryptor.finalize()
-        return base64.b64encode(encrypted_text)
-
-    @classmethod
-    def decrypt(cls, text: AnyStr, key: AnyStr) -> bytes:
-        key_bytes = _fill_bytes(key)
-        ciphertext = base64.b64decode(_as_bytes(text))
-
-        iv = ciphertext[:cls.BLOCK_SIZE]
-        text_bytes = ciphertext[cls.BLOCK_SIZE:]
-
-        cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv))
-
-        decryptor = cipher.decryptor()
-        decrypted_text = decryptor.update(text_bytes) + decryptor.finalize()
-
-        # unpad decrypted_text to get the actual text
-        decrypted_text_len = len(decrypted_text)
-        unpadding = int(decrypted_text[decrypted_text_len - 1])
-        return decrypted_text[:(decrypted_text_len - unpadding)]
+SPRIG_AES_BLOCK_SIZE = 16
 
 
-# shortcut to encryption method
-sprig_encrypt_aes = SprigAES.encrypt
+def sprig_encrypt_aes(text: AnyStr, key: AnyStr) -> bytes:
+    key_bytes = _fill_bytes(key)
+    text_bytes = _as_bytes(text)
 
-# shortcut to decryption method
-sprig_decrypt_aes = SprigAES.decrypt
+    # fill the block size of text_bytes
+    padding = SPRIG_AES_BLOCK_SIZE - len(text_bytes) % SPRIG_AES_BLOCK_SIZE
+    text_bytes += bytes(chr(padding) * padding, "utf-8")
+
+    iv = os.urandom(SPRIG_AES_BLOCK_SIZE)
+    # Sprig Go prepend the ciphertext with iv
+    ciphertext = iv + text_bytes
+    cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv))
+
+    encryptor = cipher.encryptor()
+    encrypted_text = encryptor.update(ciphertext) + encryptor.finalize()
+    return base64.b64encode(encrypted_text)
+
+
+def sprig_decrypt_aes(text: AnyStr, key: AnyStr) -> bytes:
+    key_bytes = _fill_bytes(key)
+    ciphertext = base64.b64decode(_as_bytes(text))
+
+    iv = ciphertext[:SPRIG_AES_BLOCK_SIZE]
+    text_bytes = ciphertext[SPRIG_AES_BLOCK_SIZE:]
+
+    cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv))
+
+    decryptor = cipher.decryptor()
+    decrypted_text = decryptor.update(text_bytes) + decryptor.finalize()
+
+    # unpad decrypted_text to get the actual text
+    decrypted_text_len = len(decrypted_text)
+    unpadding = int(decrypted_text[decrypted_text_len - 1])
+    return decrypted_text[:(decrypted_text_len - unpadding)]
